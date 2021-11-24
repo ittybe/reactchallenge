42241550aaffaa15950c9d5bd8d5f1134ad21038
michael <ittybemain@gmail.com>
2021-11-24 14:05:42 +0100

    Updates

--- static/js/main.3e717287.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptestchallengereact=this.webpackJsonptestchallengereact||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),a=n.n(s),o=(n(8),n(9),n(0));var i=function(){return Object(o.jsx)("div",{className:"App",children:"Hello world"})};a.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.3e717287.chunk.js.map
